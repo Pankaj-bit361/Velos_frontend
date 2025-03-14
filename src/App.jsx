ff7fa55d2f5ada67a232c9b0aff1d5fb650f4243
Pankaj-bit361 <pnkjvshsht1gmail.com>
2025-03-14 23:29:51 +0530

    Update_To_browser_router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
